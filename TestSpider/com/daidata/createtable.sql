ff871cbe6869a9b25892844f7834963efb9d1a85
cyouk2 <cyouk2@OH1144.jpn.ithd.local>
2017-03-01 17:02:01 +0900

    ggg

--- TestSpider/com/daidata/createtable.sql
+++ TestSpider/com/daidata/createtable.sql
@@ -1,4 +1,8 @@
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_days` AS 
 SELECT shop,playdate,taino,sum(bonus) as b,sum(ballin) as bin, TRUNCATE(IFNULL(sum(ballin)/sum(bonus),0),1) as rate
 FROM beidou.piainfototal
-group by shop,playdate,taino
+group by shop,playdate,taino;
+
+update piainfototal
+set starttotal = ballin
+where bonus = 0 and lineno > 0;
