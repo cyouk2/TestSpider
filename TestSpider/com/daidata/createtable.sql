5239df4ca745c21301874915afde6643dad90f4a
cyouk2 <cyouk2@OH1144.jpn.ithd.local>
2017-03-03 13:14:32 +0900

    ff

--- TestSpider/com/daidata/createtable.sql
+++ TestSpider/com/daidata/createtable.sql
@@ -1,64 +1,17 @@
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `pro_prepare_data_c`()
-BEGIN
+CREATE TABLE `traindata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datas` varchar(5000) DEFAULT NULL,
+  `rate` varchar(4) DEFAULT NULL,
+  `bonuslv` varchar(45) DEFAULT NULL,
+  `playdate` varchar(45) DEFAULT NULL,
+  `taino` varchar(45) DEFAULT NULL,
+  `shop` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1270 DEFAULT CHARSET=utf8;
 
-	DECLARE done INT DEFAULT FALSE;
-    DECLARE strShop varchar(45) DEFAULT '';
-    DECLARE strTaino varchar(45) DEFAULT '';
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_lines` AS select `piainfototal`.`shop` AS `shop`,`piainfototal`.`taino` AS `taino`,count(0) AS `liness` from `piainfototal` group by `piainfototal`.`shop`,`piainfototal`.`taino` order by `liness` desc limit 500;
 
-    begin
-    	declare mycur cursor for 
-			SELECT shop, taino FROM v_lines;
-        
-		DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-		open mycur;
-		read_loop: LOOP
-			FETCH mycur INTO strShop,strTaino;
-				call pro_prepare_data_b(strShop,strTaino);
-			IF done THEN
-				LEAVE read_loop;
-			END IF;
-		END LOOP;
-		CLOSE mycur;
-	end;
-END$$
-DELIMITER ;
-
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `pro_prepare_data_b`(IN shopid INT,IN tainoid INT)
-BEGIN
-
-	DECLARE done INT DEFAULT FALSE;
-	DECLARE limiter INT DEFAULT 0;
-    DECLARE strPlayDate varchar(45) DEFAULT '';
-
-    begin
-    	declare mycur cursor for 
-			SELECT distinct playdate FROM beidou.piainfototal
-			where shop=shopid and taino=tainoid;
-        
-		DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-		open mycur;
-		read_loop: LOOP
-			FETCH mycur INTO strPlayDate;
-            	begin
-					SELECT 
-						COUNT(*)
-					INTO limiter FROM beidou.piainfototal
-					WHERE shop = shopid AND taino = tainoid AND playdate < strPlayDate;
-					IF limiter < 31 THEN
-                    ITERATE read_loop;
-                    END IF;
-				end;
-				call pro_prepare_data(shopid,tainoid,strPlayDate);
-			IF done THEN
-				LEAVE read_loop;
-			END IF;
-		END LOOP;
-		CLOSE mycur;
-	end;
-END$$
-DELIMITER ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_rate_info` AS select `piainfototal`.`shop` AS `shop`,`piainfototal`.`taino` AS `taino`,`piainfototal`.`playdate` AS `playdate`,sum(`piainfototal`.`bonus`) AS `bonus`,sum(`piainfototal`.`starttotal`) AS `starts`,truncate(ifnull((sum(`piainfototal`.`starttotal`) / sum(`piainfototal`.`bonus`)),0),1) AS `rate` from `piainfototal` group by `piainfototal`.`shop`,`piainfototal`.`playdate`,`piainfototal`.`taino` order by `piainfototal`.`shop`,`piainfototal`.`taino`,`piainfototal`.`playdate`;
 
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `pro_prepare_data`(IN shopid INT,IN tainoid INT,IN strPlayDate varchar(45))
@@ -111,26 +64,67 @@ BEGIN
 END$$
 DELIMITER ;
 
-SELECT `v_rate_info`.`shop`,
-    `v_rate_info`.`taino`,
-    `v_rate_info`.`playdate`,
-    `v_rate_info`.`bonus`,
-    `v_rate_info`.`starts`,
-    `v_rate_info`.`rate`
-FROM `beidou`.`v_rate_info`;
 
-SELECT `v_lines`.`shop`,
-    `v_lines`.`taino`,
-    `v_lines`.`liness`
-FROM `beidou`.`v_lines`;
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `pro_prepare_data_b`(IN shopid INT,IN tainoid INT)
+BEGIN
 
-CREATE TABLE `traindata` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `datas` varchar(5000) DEFAULT NULL,
-  `rate` varchar(4) DEFAULT NULL,
-  `bonuslv` varchar(45) DEFAULT NULL,
-  `playdate` varchar(45) DEFAULT NULL,
-  `taino` varchar(45) DEFAULT NULL,
-  `shop` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1270 DEFAULT CHARSET=utf8;
+	DECLARE done INT DEFAULT FALSE;
+	DECLARE limiter INT DEFAULT 0;
+    DECLARE strPlayDate varchar(45) DEFAULT '';
+
+    begin
+    	declare mycur cursor for 
+			SELECT distinct playdate FROM beidou.piainfototal
+			where shop=shopid and taino=tainoid;
+        
+		DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+		open mycur;
+		read_loop: LOOP
+			FETCH mycur INTO strPlayDate;
+            	begin
+					SELECT 
+						COUNT(*)
+					INTO limiter FROM beidou.piainfototal
+					WHERE shop = shopid AND taino = tainoid AND playdate < strPlayDate;
+					IF limiter < 31 THEN
+                    ITERATE read_loop;
+                    END IF;
+				end;
+				call pro_prepare_data(shopid,tainoid,strPlayDate);
+			IF done THEN
+				LEAVE read_loop;
+			END IF;
+		END LOOP;
+		CLOSE mycur;
+	end;
+END$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `pro_prepare_data_c`()
+BEGIN
+
+	DECLARE done INT DEFAULT FALSE;
+    DECLARE strShop varchar(45) DEFAULT '';
+    DECLARE strTaino varchar(45) DEFAULT '';
+
+    begin
+    	declare mycur cursor for 
+			SELECT shop, taino FROM v_lines;
+        
+		DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+		open mycur;
+		read_loop: LOOP
+			FETCH mycur INTO strShop,strTaino;
+				call pro_prepare_data_b(strShop,strTaino);
+			IF done THEN
+				LEAVE read_loop;
+			END IF;
+		END LOOP;
+		CLOSE mycur;
+	end;
+END$$
+DELIMITER ;
+
